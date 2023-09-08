0ddfa73f50b1cdecc88266659c53f0e4c76dd487
Diego Lara <diegolaradeandres@gmail.com>
2023-09-08 09:44:33 +0100

    feat: Cascade python solver

---
+++ R/grt_al_py.R
@@ -0,0 +1,366 @@
+#' GTR Adaptive Lasso Function.
+#'
+#' @description This is a wrap-up function for adaptive lasso GTR/spline-spline. It allows for a Lasso first-step.
+#'
+#' @param y
+#' @param x
+#' @param X.type
+#' @param gam
+#' @param lam.vec
+#' @param xdf
+#' @param ydf
+#' @param yorder
+#' @param nyg.max
+#' @param Ysing
+#' @param y_knots
+#' @param ng.qgm
+#' @param addxint
+#' @param info
+#' @param yorth
+#' @param xorth
+#' @param reltol
+#' @param feastol
+#' @param abstol
+#' @param tol.res
+#' @param cval
+#' @param lb
+#' @param threshold
+#' @param e0mode
+#' @param unconstrained
+#' @param constrained
+#' @param AL
+#' @param doprimal
+#' @param fac
+#' @param maxit
+#' @param model
+#' @param bounded
+#' @param beta2
+#' @param Cbound
+#' @param ugrid
+#' @param gam.grid
+#' @param algor
+#' @param coord.bare
+#' @param coord.spline
+#' @param coord.tensor
+#' @param easy
+#' @param method
+#' @param delta.ok
+#' @param parallel
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gtr_al <- function(y,x,X.type=NULL,gam=0,lam.vec=rep(1,6),xdf=0,ydf=4,yorder=4,nyg.max=101,Ysing=FALSE,y_knots=NULL,ng.qgm=101,
+                   addxint=T,info=NULL,yorth=FALSE,xorth=FALSE,reltol=1.e-03,feastol=1.e-03,abstol=1.e-03,tol.res=1e-1,cval=1e-1,lb=1,threshold=1e-5,
+                   e0mode=F,constrained=F,AL=F,doprimal=F,fac=1,maxit=90,model=NULL,bounded=F,beta2=F,Cbound=Inf,ugrid=seq(.1,.9,by=.1),
+                   gam.grid=log_space(-3,3,20),algor="ECOS",coord.bare=NULL,coord.spline=NULL,coord.tensor=NULL,easy=T,method="BIC",
+                   delta.ok=F,parallel=F,eta.check=T){
+
+  objval.bic  <- NULL
+  res.al.bic  <- NULL
+  mdedy       <- NULL
+  status      <- NULL
+
+  if(ydf==0 || xdf==0){ cval <- 0 }
+
+  if(Cbound==Inf){ bounded = F }
+
+  if(length(model)==0){
+
+    nobs       <- length(y)
+    b2min.now  <- -1
+    nyg.now    <- 0
+    if(!beta2){ b2min <- 1 }
+
+    if(ydf==0){yorder<-2} # tmp patch ss
+    ifelse(Ysing || ydf==0, nYS <- ydf+yorder, nYS <- ydf+yorder-1)
+    # Generate X data types if needed
+    if(is.null(X.type)){ X.type <- rep("continuous",NCOL(x)) }
+    # Check that each spline component as xdf defined:
+    if(length(coord.spline)>0){stopifnot(length(xdf)==length(coord.spline))}
+
+    xgrid.qgm  <- grids_func(x=x, X.type=X.type, nxgrid=ng.qgm, gridx.cont=T)
+    ygrid.qgm  <- seq(min(y),max(y),len=ng.qgm)
+
+    res0       <- NULL
+    res.sol    <- NULL
+    res.al     <- NULL
+    BIC.vec    <- NULL
+    BIC.vec2   <- NULL
+    fac.now2   <- NULL
+    gam.al     <- NULL
+    xknots     <- NULL
+    cvg        <- NULL
+
+    if(length(info)==0){ info <- data_info(x=x, X.type=X.type, xdf=xdf, coord.bare=coord.bare, coord.spline=coord.spline, coord.tensor=coord.tensor, nxgrid=ng.qgm, delta.ok=delta.ok) }
+    if(dim(x)[2] == 1 && all(x[,1] == 1)){
+      datmat     <- data_prep(y=as.numeric(y),x=x,ygrid=ygrid.qgm,
+                              Xs=x,nxgrid=0,
+                              y_knots=y_knots,ydf=ydf,addxint=addxint,yorder=yorder,
+                              yorth=yorth,xorth=xorth,Ysing=Ysing,e0mode=e0mode,nygrid=ng.qgm)
+    }else{
+      datmat     <- data_prep(y=as.numeric(y),x=x,ygrid=ygrid.qgm,xgrid=xgrid.qgm,
+                              info=info,y_knots=y_knots,ydf=ydf,addxint=addxint,yorder=yorder,
+                              yorth=yorth,xorth=xorth,Ysing=Ysing,e0mode=e0mode,nxgrid=ng.qgm,nygrid=ng.qgm)
+    }
+
+    TYX        <- datmat$TZ;
+    tYX        <- datmat$tZ
+    Xs         <- datmat$Xs
+    nXs        <- ncol(datmat$Xs);
+    nYS        <- ncol(datmat$YS)
+    Xsgrid.qgm <- datmat$Xsgrid
+    sYgrid.qgm <- datmat$sYgrid
+    rm(datmat)
+
+
+    # ===============
+    # Estimation
+    # ===============
+
+    dedy.min    <- -1
+    cvg.mono    <- NULL
+    nyg         <- -1/2
+    #easy        <- F
+    if(mean(xdf) == 0){ easy <- T }
+
+    while( dedy.min <= .Machine$double.eps && nyg < nyg.max ){
+      #while( dedy.min <= cval && nyg < nyg.max ){
+
+      if(nyg>201 && easy==F && xdf !=0){ easy <- T; nyg <- 1}
+      nyg     <- 2*nyg + 1
+      if(nyg==1){ nyg <- 3 }; cat(paste("nyg now =",nyg,"\n"))
+      if(nyg==0){
+        Xsgrid <- Xs; #rm(Xs);
+        sYgrid <- NULL
+        if(dim(x)[2] == 1 && all(x[,1] == 1)){sYgrid <- sYgrid.qgm}
+      }
+
+      # Generate grid of Ys for QGM constraints
+      if(nyg>0){
+        nygnow <- 0+nyg
+        xgrid  <- grids_func(x=x, X.type=X.type, nxgrid=nygnow, gridx.cont=T)
+        ygrid  <- seq(min(y),max(y),len=nygnow)
+        if(dim(x)[2] == 1 && all(x[,1] == 1) ){
+          datmat <- data_prep(y=as.numeric(y),x=x,ygrid=ygrid,
+                              Xs=x,nxgrid=0,
+                              y_knots=y_knots,ydf=ydf,addxint=addxint,yorder=yorder,
+                              yorth=yorth,xorth=xorth,Ysing=Ysing,e0mode=e0mode)
+        }else{
+          datmat <- data_prep(y=as.numeric(y),x=x,ygrid=ygrid,xgrid=xgrid,
+                              info=info,y_knots=y_knots,ydf=ydf,addxint=addxint,yorder=yorder,
+                              yorth=yorth,xorth=xorth,Ysing=Ysing,e0mode=e0mode,nxgrid=nygnow)
+        }
+
+        Xsgrid <- datmat$Xsgrid
+        sYgrid <- datmat$sYgrid
+        rm(datmat)
+      }
+
+      ans <- gtr_compute_py(nyg=nyg,TYX=TYX,tYX=tYX,gam=gam,reltol=reltol,feastol=feastol,abstol=abstol,
+                          doprimal=doprimal,lam.vec=lam.vec,
+                          y=y,x=x,info=info,yorder=yorder,ydf=ydf,
+                          Ysing=Ysing,nYS=nYS,nXs=nXs,
+                          xgrid.qgm=xgrid.qgm,
+                          Xs=Xsgrid,sYgrid=sYgrid,ugrid=ugrid,
+                          Xs.qgm=Xsgrid.qgm,sYgrid.qgm=sYgrid.qgm,
+                          maxit=maxit,ng.qgm=ng.qgm,dedy.min=dedy.min,
+                          tol.res=reltol,algor=algor,easy=easy,e0mode=e0mode,eta.check=eta.check )
+
+      dedy.min    <- ifelse(length(ans$res.sol$eta)>0,min(ans$res.sol$eta,ans$dedy.min),ans$dedy.min)
+      tol.res     <- ans$tol.res
+      cvg.mono    <- ans$cvg.mono
+      algor       <- ans$algor
+
+      if(constrained == F){ nyg <- nyg.max+1 }   # This is to exit the loop with increasing number of QGM constraints
+
+    }
+
+    if(parallel){
+      res.sol <- list(bmat=ans$res.sol$bmat, eta=ans$res.sol$eta)
+      rm(ans)
+    }
+    if(!parallel){
+      res.sol  <- ans$res.sol;
+      nyg.now  <- ans$nyg.now;
+      cvg      <- ans$cvg;
+      fac.now2 <- ans$fac.now2;
+    }
+
+    if(dim(x)[2] == 1 && all(x[,1] == 1)){
+      datmat.now <- data_prep(y=as.numeric(y),x=x, y_knots=y_knots,ydf=ydf, Xs=x,nxgrid=0,
+                              addxint=addxint,yorder=yorder,yorth=FALSE,xorth=FALSE,
+                              Ysing=Ysing,e0mode=e0mode,returnTZ=T,returnTZg=F)
+    }
+    else{
+      datmat.now <- data_prep(y=as.numeric(y),x=x, info=info,y_knots=y_knots,ydf=ydf,
+                              addxint=TRUE,yorder=yorder,yorth=FALSE,xorth=FALSE,
+                              Ysing=Ysing,e0mode=e0mode,returnTZ=T,returnTZg=F)
+    }
+
+    ################ STORE FIRST STEP RESULTS ###########################
+
+    if( !parallel && (length(res.sol)>0 && min(res.sol$eta)>.Machine$double.eps )
+        || parallel && (length(res.sol$bmat)>0 && min(res.sol$eta)>.Machine$double.eps)){
+      nXs       <- ncol(datmat.now$Xs)
+      if(gam==0){ BIC.vec   <- tic_func(res=res.sol,TZ=datmat.now$TZ,tZ=datmat.now$tZ,method=method,Ginv=T) }
+      if(gam>.Machine$double.eps){
+        Adex <- which(abs(res.sol$bmat)>=threshold)
+        BIC.vec <- tic_func(res=res.sol,TZ=datmat.now$TZ,tZ=datmat.now$tZ,Adex=Adex,method=method,Ginv=T)
+      }
+    }
+    rm(datmat.now)
+  }
+
+
+  ################### BEGIN ADAPTIVE LASSO ##########################
+
+  # ===============================================
+  # Retrieve info if gtr.al() was previously run separately without adaptive lasso
+
+  if(length(model)>0){
+
+    res.sol   <- model$res
+    res.al    <- NULL
+    gam.al    <- NULL
+    BIC.vec   <- model$BIC
+    BIC.vec2  <- NULL
+    nXs       <- model$nr
+    nYS       <- model$nc
+    fac.now2  <- model$fac
+    xknots    <- model$xknots
+    nyg.now   <- model$nyg
+    cvg       <- model$cvg
+    Xs        <- model$Xs
+  }
+
+  # ===============================================
+
+  if( AL && min(res.sol$eta)>0 ){
+
+    doprimal <- T
+    nglam    <- length(gam.grid)
+
+    # Adaptive Lasso
+    ans0      <- list()
+
+    # Generate X data types if needed
+    if(is.null(X.type)){ X.type <- rep("continuous",NCOL(x)) }
+    xgrid.qgm  <- grids_func(x=x, X.type=X.type, nxgrid=ng.qgm, gridx.cont=T)
+    ygrid.qgm  <- seq(min(y),max(y),len=ng.qgm)
+    if(length(info)==0){ info <- data.info(x=x, X.type=X.type, xdf=xdf, coord.bare=coord.bare, coord.spline=coord.spline, coord.tensor=coord.tensor, nxgrid=ng.qgm, delta.ok=delta.ok) }
+    datmat     <- data_prep(y=as.numeric(y),x=x,ygrid=ygrid.qgm,xgrid=xgrid.qgm,bmat=matrix(res.sol$bmat,nr=nXs,nc=nYS),
+                            info=info,y_knots=y_knots,ydf=ydf,addxint=addxint,yorder=yorder,
+                            yorth=yorth,xorth=xorth,Ysing=Ysing,e0mode=e0mode,nxgrid=ng.qgm,nygrid=ng.qgm)
+    TYX        <- datmat$TZ;
+    tYX        <- datmat$tZ
+    Xs         <- datmat$Xs
+    Xsgrid.qgm <- datmat$Xsgrid
+    sYgrid.qgm <- datmat$sYgrid
+
+    # ===============
+    # Estimation
+    # ===============
+    ans0  <- list()
+    ggnow <- 0
+
+    for(gg in 1:nglam){
+      cat( paste("gg =",gg,"\n") )
+
+      dedy.min    <- -1
+      nyg         <- -1/2
+      if(mean(xdf) == 0){ easy <- T }
+
+      while( dedy.min <= .Machine$double.eps && nyg < nyg.max ){
+
+        if(nyg>201 && easy==F && xdf !=0){ easy <- T; nyg <- 1}
+        nyg     <- 2*nyg + 1
+        if(nyg==1){ nyg <- 3 }; cat(paste("nyg now =",nyg,"\n"))
+        if(nyg==0){
+          Xsgrid <- Xs; #if(gg==5){rm(Xs)};
+          sYgrid <- NULL
+        }
+
+        # Generate grid of Ys for QGM constraints
+        if(nyg>0){
+          xgrid  <- grids_func(x=x, X.type=X.type, nxgrid=nyg, gridx.cont=T)
+          ygrid  <- seq(min(y),max(y),len=nyg)
+          datmat <- data_prep(y=as.numeric(y),x=x,ygrid=ygrid,xgrid=xgrid,
+                              info=info,y_knots=y_knots,ydf=ydf,addxint=addxint,yorder=yorder,
+                              yorth=yorth,xorth=xorth,Ysing=Ysing,e0mode=e0mode,nxgrid=nyg)
+          Xsgrid <- datmat$Xsgrid
+          sYgrid <- datmat$sYgrid
+          rm(datmat)
+        }
+
+        weights <- as.vector(abs(res.sol$bmat))^1
+        zeros   <- which(weights<threshold)
+        weights <- 1/as.numeric(weights)
+        TYXnow  <- TYX
+        tYXnow  <- tYX
+        if(length(zeros>0)){
+          weights <- weights[-zeros]
+          TYXnow  <- TYX[,-zeros]
+          tYXnow  <- tYX[,-zeros]
+        }
+
+        ans     <- gtr_compute_py( nyg=nyg,TYX=TYXnow,tYX=tYXnow,gam=gam.grid[gg],weights=weights,zeros=zeros,
+                                reltol=reltol,feastol=feastol,abstol=abstol,
+                                doprimal=doprimal,lam.vec=lam.vec,
+                                y=y,x=x,info=info,yorder=yorder,ydf=ydf,
+                                Ysing=Ysing,nYS=nYS,nXs=nXs,
+                                xgrid.qgm=xgrid.qgm,
+                                Xs=Xsgrid,sYgrid=sYgrid,ugrid=ugrid,
+                                Xs.qgm=Xsgrid.qgm,sYgrid.qgm=sYgrid.qgm,
+                                maxit=maxit,ng.qgm=ng.qgm,dedy.min=dedy.min,
+                                tol.res=reltol,algor=algor,easy=easy,threshold=threshold,e0mode=e0mode,eta.check=eta.check )
+
+        dedy.min <- ans$dedy.min
+
+        if(constrained == F){ nyg <- nyg.max+1 }   # This is to exit the loop with increasing number of QGM constraints
+
+      }
+
+      if(length(ans$res.sol)>0){
+        ggnow         <- ggnow + 1
+        ans0[[ggnow]] <- ans$res.sol;
+        if(length(zeros)>0){
+          TYXnow <- TYX[,-zeros]
+          tYXnow <- tYX[,-zeros]
+        }
+        BIC.now       <- tic_func(res=ans0[[ggnow]],TZ=TYXnow,tZ=tYXnow,method=method,Ginv=T)
+        objval.bic    <- c(objval.bic, BIC.now)
+      }
+    }
+
+    select.bic <- mstar.fun(round(objval.bic,digits=1))
+
+    res.al     <- ans0[[select.bic]]
+    BIC.vec2   <- objval.bic[select.bic]
+    gam.al     <- gam.grid[select.bic]
+
+    if(length(zeros)>0){
+      M            <- matrix(0,nXs*nYS,nXs*nYS-length(zeros))
+      M[-zeros,]   <- diag(1,nXs*nYS-length(zeros),nXs*nYS-length(zeros))
+      res.al$bmat  <- M%*%res.al$bmat
+    }
+
+  }
+
+  if(!parallel){
+    res    <- list(bmat=res.sol$bmat,e=res.sol$e,eta=res.sol$eta)
+    res.al <- list(bmat=res.al$bmat,e=res.al$e,eta=res.al$eta)
+    if(length(res.sol)==0){ nXs <- NULL }
+  }
+
+  ifelse(parallel, ans    <- list(BIC=BIC.vec, bmat=res.sol$bmat),
+         ans    <- list(res=res,res.al=res.al,gam.al=gam.al,BIC=BIC.vec,BIC.al=BIC.vec2,
+                        nr=nXs,nc=nYS,fac=fac.now2,xknots=xknots,nyg=nyg.now,cvg=cvg,
+                        mdedy=mdedy,objval.bic=objval.bic,algor=algor,status=res.sol$result$status,
+                        X.type=X.type,coord.bare=coord.bare,coord.spline=coord.spline,coord.tensor=coord.tensor))
+
+
+  return(ans)
+
+}
---
+++ R/gtr_compute_py.R
@@ -0,0 +1,303 @@
+#' GTR Compute Function using python
+#'
+#' @description This function does the inner computation for GTR. Used to be called "inner gtr".
+#'
+#' @param TYX
+#' @param tYX
+#' @param y
+#' @param x
+#' @param nyg
+#' @param ng.qgm
+#' @param weights
+#' @param zeros
+#' @param reltol
+#' @param feastol
+#' @param abstol
+#' @param pen
+#' @param cvg.mono
+#' @param res.sol
+#' @param fac
+#' @param fac.now2
+#' @param Xs
+#' @param sYgrid
+#' @param xgrid.qgm
+#' @param Xs.qgm
+#' @param sYgrid.qgm
+#' @param info
+#' @param yorder
+#' @param ydf
+#' @param y_knots
+#' @param Ysing
+#' @param maxit
+#' @param nXs
+#' @param nYS
+#' @param lam.vec
+#' @param gam
+#' @param ugrid
+#' @param dedy.min
+#' @param doprimal
+#' @param tol.res
+#' @param bounded
+#' @param Cbound
+#' @param cval
+#' @param algor
+#' @param easy
+#' @param threshold
+#' @param e0mode
+#'
+#' @return
+#' @export
+#'
+#'
+#' @importFrom outliers grubbs.test
+#' @examples
+gtr_compute <- function(TYX,tYX,y,x,
+                        nyg=0,ng.qgm,weights=1,zeros=NULL,
+                        reltol=1e-3,feastol=1e-3,abstol=1e-3,pen=NULL,
+                        cvg.mono=NULL,res.sol=NULL,fac=1,fac.now2=NULL,
+                        Xs=NULL,sYgrid=NULL,
+                        xgrid.qgm=NULL,Xs.qgm=NULL,sYgrid.qgm=NULL,
+                        info,yorder,ydf,y_knots=NULL,Ysing,maxit,nXs,nYS,
+                        lam.vec=NULL,gam=0,ugrid=NULL,dedy.min=NULL,doprimal=F,
+                        tol.res,bounded=F,Cbound=Inf,cval=1e-1,algor="ECOS",easy=T, threshold=1e-5,e0mode=F, eta.check = F){
+
+  # =========
+  setup <- matrix(0,2,2) # Keep track of algo and problem formulation
+  rownames(setup) <- c("ECOS","SCS")
+  colnames(setup) <- c("Primal","Dual")
+  if(algor=="ECOS"){
+    ifelse(doprimal, setup[1,1] <- 1, setup[1,2] <- 1)
+  }
+  else{
+    ifelse(doprimal, setup[2,1] <- 1, setup[2,1] <- 1)
+  }
+  # =========
+
+  M <- diag(1,nXs*nYS,nXs*nYS)
+  nzeros <- length(zeros)
+  if(nzeros>0){
+    M            <- matrix(0,nXs*nYS,nXs*nYS-nzeros)
+    M[-zeros,]   <- diag(1,nXs*nYS-nzeros,nXs*nYS-nzeros)
+  }
+
+  cvg         <- NULL
+  fac.now     <- fac
+  res0        <- 1
+  nyg.now     <- 0
+  exit        <- F
+
+  while(length(cvg)==0 && tol.res*fac.now < 10 && length(res0)>0 && !exit){ # until cvg converges (to 1)
+
+    cat(paste("fac =",fac.now,"\n"))
+    res0 <- NULL
+    if(nyg != 0){ pen <- T }
+    if(length(pen)>0 && doprimal == F){
+      cat("Switching to primal\n")
+      doprimal <- T
+    }
+
+    try(res0 <- gtr_solve_py(TYX=TYX,tYX=tYX))
+
+    if(length(res0)<=1 || is.na(res0$llf) || (!(dim(x)[2] == 1 && all(x[,1] == 1)) && length(unique(round(res0$eta,digits=3)))==1)){
+      # Try a different algorithm
+      cat("Trying a different algorithm\n")
+      ifelse(algor=="ECOS", algor<-"SCS", algor<-"ECOS"); ifelse(algor=="SCS", tol.res <- 1.e-01, tol.res <- 1e-4);
+      try(res0 <- gtr_solve_py(TYX=TYX,tYX=tYX))
+      if(eta.check && length(pen)==0
+         && (is.na(res0$llf) || length(res0)<=1 || ((!(dim(x)[2] == 1 && all(x[,1] == 1)) && length(unique(round(res0$eta,digits=3)))==1)))){
+        exit <- T
+        cat("No algorithm worked\n")
+      }
+
+    }
+
+
+    # Check if high eta is not a computational issue
+    # ----------------------------------------------
+    if(length(res0)>1 && eta.check && !exit && gam == 0){
+      #if(mean((res0$eta^2-mean(res0$eta^2))^2)>mean(res0$eta^2)){ exit <- T }
+      gtest<-grubbs.test(log(res0$eta))
+      print(paste("grubbs = ",gtest$p.value))
+      if(gtest$p.value<.05){
+        if(length(pen)==0){
+          #print(sd(res0$eta))
+          if(!is.na(res0$llf)){
+            cat("dgap check in progress\n")
+            doprimal.now <- !doprimal
+            res.dual <- NULL
+            try(res.dual <- gtr_solve_py(TYX=TYX,tYX=tYX))
+
+            if(length(res.dual)>1 && !is.na(res.dual$llf)){
+              print(abs(res.dual$llf - res0$llf))
+              if(abs(res.dual$llf - res0$llf) > 1e-1 || (sd(res0$eta)>1 && abs(res.dual$llf - res0$llf) > 1e-6)){
+                print(paste("dgap now =",abs(res.dual$llf - res0$llf)))
+                exit<-T
+              }#else{
+              #if(res.dual$llf > res0$llf){
+              #   res0 <- res.dual
+              #   }
+              #}
+            }
+            if(length(res.dual)<=1){
+              exit <- T
+              res0<-NULL
+              cat("dgap check not available\n")
+            }
+          }
+        }else{
+          # Try a different algorithm
+          res.alg2 <- NULL
+          ifelse(algor=="ECOS", algor<-"SCS", algor<-"ECOS"); ifelse(algor=="SCS", tol.res <- 1.e-01, tol.res <- 1e-4);
+          try(res.alg2 <- gtr_solve_py(TYX=TYX,tYX=tYX))
+
+          if(length(res.alg2)>1){
+            #if(length(res.alg2$eta)>0 && mean((res.alg2$eta^2-mean(res.alg2$eta^2))^2)<mean(res.alg2$eta^2)){
+            if(!is.na(res.alg2$llf)){
+              print(abs(res.alg2$llf - res0$llf))
+              if(abs(res.alg2$llf - res0$llf) < 1e-1){
+                if(res.alg2$llf > res0$llf){
+                  cat("switching algo.\n")
+                  res0 <- res.alg2
+                }
+              }else{
+                exit<-T
+                res0<-NULL
+              }
+              #}
+            }
+          }else{
+            exit<-T
+            res0<-NULL
+          }
+        }
+      }
+    }
+
+    #print(exit)
+    if(!exit){
+
+      if(length(res0)>1 && ((dim(x)[2] == 1 && all(x[,1] == 1)) || length(unique(round(res0$eta,digits=3)))!=1)){
+        #print(sd(res0$eta))
+        if(res0$result$num_iters==maxit){ fac <- fac*2 } # not converged
+        # Check integrates to 1
+        if(nYS>2 && nXs >1){
+
+          if(!is.list(Xs)){
+            b2min <- beta_check(bmat=matrix(M%*%res0$bmat,nr=nXs,nc=nYS),Xs=Xs,nXs=nXs,nYS=nYS)$b2min
+          }
+          if(is.list(Xs)){
+            b2min <- NULL
+            for(i in 1:length(Xs)){
+              for(j in 1:dim(Xs[[i]])[3]){
+                # b2min is the smallest value of beta2(X). It should be strictly positive.
+                b2min <- c(b2min,beta_check(bmat=matrix(M%*%res0$bmat,nr=nXs,nc=nYS),Xs=Xs[[i]][,,j],nXs=nXs,nYS=nYS)$b2min)
+              }
+            }
+            b2min <- min(b2min)
+          }
+
+          if(b2min<=.Machine$double.eps){
+            try( res0 <- gtr_solve_py(TYX=TYX,tYX=tYX))
+          }
+
+          if(length(res0)==1
+             || length(res0$result$status)==0
+             || res0$result$status=="optimal_inaccurate"
+             || length(which(round(res0$eta,digits=3)==cval))>0
+             || (!(dim(x)[2] == 1 && all(x[,1] == 1)) && length(unique(round(res0$eta,digits=3)))==1)){
+
+            # Try a different algorithm
+            ifelse(algor=="ECOS",algor<-"SCS",algor<-"ECOS"); ifelse(algor=="SCS", tol.res <- 1.e-01, tol.res <- 1e-4);
+            try( res0 <- gtr_solve_py(TYX=TYX,tYX=tYX))
+          }
+
+        }
+
+        if(length(res0)>1 && ((dim(x)[2] == 1 && all(x[,1] == 1)) || length(unique(round(res0$eta,digits=3)))!=1)){
+
+          # Check QGM
+          if(nXs > 1){
+            if(nYS>2){
+              datmat.mono <- qgm_check(y=y,x=x,xgrid=xgrid.qgm,res=res0,Ysing=Ysing,y_knots=y_knots,ugrid=ugrid,ng.qgm=ng.qgm,nxgrid=ng.qgm,nygrid=ng.qgm,nXs=nXs,nYS=nYS,ydf=ydf,yorder=yorder,info=info,easy=T,zeros=zeros,e0mode=e0mode)
+              mdedy       <- min(datmat.mono$dedygrid)
+              if( mdedy>.Machine$double.eps && !easy ){
+                cat("Calling qgm_check\n")
+                t_start <- Sys.time()
+                datmat.mono <- qgm_check(y=y,x=x,xgrid.qgm=xgrid.qgm,res=res0,Ysing=Ysing,y_knots=y_knots,ugrid=ugrid,ng.qgm=ng.qgm,nxgrid=ng.qgm,nygrid=ng.qgm,nXs=nXs,nYS=nYS,ydf=ydf,yorder=yorder,info=info,easy=F,zeros=zeros,e0mode=e0mode)
+                t_end <- Sys.time()
+                t_check <- t_end - t_start
+                cat(paste("It took =",round(t_check,digits=3),"\n"))
+                mdedy       <- min(datmat.mono$dedygrid)
+              }
+              rm(datmat.mono)
+              cat(paste("min dedy now is =",round(mdedy,digits=6),"\n"))
+            }
+
+            if(nYS==2){
+              if(!is.list(Xs.qgm)){
+                mdedy <- beta_check(bmat=matrix(M%*%res0$bmat,nr=nXs,nc=nYS),Xs=Xs.qgm,nXs=nXs,nYS=nYS)$b2min
+              }
+              if(is.list(Xs.qgm)){
+                b2min <- NULL
+                for(i in 1:length(Xs.qgm)){
+                  for(j in 1:dim(Xs.qgm[[i]])[3]){
+                    # b2min is the smallest value of beta2(X). It should be strictly positive.
+                    b2min <- c(b2min,beta_check(bmat=matrix(M%*%res0$bmat,nr=nXs,nc=nYS),Xs=Xs.qgm[[i]][,,j],nXs=nXs,nYS=nYS)$b2min)
+                  }
+                }
+                mdedy <- min(b2min)
+              }
+              cat(paste("min dedy now is =",round(mdedy,digits=6),"\n"))
+            }
+          }
+
+          if(nXs == 1){
+
+            b2min <- sYgrid%*%matrix(M%*%res0$bmat,nr=nYS,nc=1)
+            mdedy <- min(b2min)
+
+            cat(paste("min dedy now is =",round(mdedy,digits=6),"\n"))
+          }
+
+          #      if(mdedy>.Machine$double.eps){
+
+          if(((length(cvg.mono)==0 || (length(cvg.mono)>0 && mdedy>.Machine$double.eps) ) ) ){
+
+            if(length(cvg.mono)==0){
+
+              cvg      <- 1
+              nyg.now  <- nyg
+              cat(paste("min dedy now =",round(mdedy,digits=6),"\n"))
+
+            }
+
+            if(mdedy>.Machine$double.eps){
+
+              cvg.mono <- 1
+              res.sol  <- res0
+
+              fac.now2 <- fac.now
+              cat(paste("fac.now =",fac.now2,"\n"))
+              dedy.min <- mdedy
+              nyg.now  <- nyg
+
+            }
+
+            #        }
+
+          }
+
+        }
+
+      }
+
+      ifelse(algor=="ECOS" || nyg == 0, fac.now <- 11/tol.res, fac.now <- 2*fac*fac.now)
+
+    }
+
+  }
+
+  ans <- list(res.sol=res.sol,res0=res0,cvg=cvg,nyg.now=nyg.now,fac.now2=fac.now2,dedy.min=dedy.min,algor=algor,tol.res=tol.res)
+
+  return(ans)
+}
